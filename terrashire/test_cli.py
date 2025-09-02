f09093d74f1680b0f25fe399b3058858cd116a72
👻 Tyler Moses 👻 <tmosest@gmail.com>
2025-09-02 03:40:57 -0700

    unit tests for making sure we at least call copy on each mod

--- terrashire/test_cli.py
+++ terrashire/test_cli.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from typer.testing import CliRunner
 
 
@@ -12,7 +13,7 @@ def test_help():
     assert "Terrashire" in result.stdout
 
 def test_init():
-    path =  os.path.join(os.path.expanduser("~"), '/terrashire-cli-test-units')
+    path =  os.path.join(os.path.expanduser("~"), 'terrashire-cli-test-units')
     result = runner.invoke(app, ["init", path])
     assert result.exit_code == 0
     assert "Initializing homelab iac" in result.stdout
@@ -27,3 +28,34 @@ def test_list_network():
     assert "Copying files from" in result.stdout
     assert "Reinitialized existing Git repository in" in result.stdout
 '''
+
+def test_gitea_init():
+    path =  os.path.join(os.path.expanduser("~"), 'terrashire-cli-test-units')
+    print(f"{path}")
+    runner.invoke(app, ["init", path])
+
+    result = runner.invoke(app, ["init", path])
+    assert result.exit_code == 0
+    
+    os.chdir(path)
+    result = runner.invoke(app, ["gitea", "init"])
+
+    assert result.exit_code == 0
+    assert "/gitea" in result.stdout
+
+def test_mods_init():
+    path =  os.path.join(os.path.expanduser("~"), 'terrashire-cli-test-units')
+    print(f"{path}")
+    runner.invoke(app, ["init", path])
+
+    result = runner.invoke(app, ["init", path])
+    assert result.exit_code == 0
+
+    mods = ["opnsense", "npm", "gitea", "pihole", "proxmox", "woodpecker"]
+    
+    for mod in mods:
+        os.chdir(path)
+        result = runner.invoke(app, [mod, "init"])
+        
+        assert result.exit_code == 0
+        assert f"/{mod}" in result.stdout
